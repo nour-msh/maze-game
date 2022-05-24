b9dbcfd6778682be2240e0f4e5536c3ef4193ed2
Nour Mshawrab <mshawrabnour@gmail.com>
2022-05-24 09:47:22 +0300

    Losing game function

--- game.js
+++ game.js
@@ -2,6 +2,28 @@
 window.onload=function(){
     var boundaries=document.getElementsByClassName("boundary");
 
+
+
+
+    function launchgame(){
+        for (let i=0; i<boundaries.length; i++){
+           boundaries[i].addEventListener("mouseenter", lose);
+        }
+    }
+
+
+    function lose(){
+        for (let i=0; i<boundaries.length; i++){
+            boundaries[i].classList.add("youlose");
+            document.getElementById("status").innerHTML="You Lost!";
+        } 
+    }
+    
+
+
+
+    
+
     document.getElementById("start").addEventListener("mouseenter", launchgame);
 
 
