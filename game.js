ced815dc9c053bd3405dee63a6ce72d1797d0bf9
Nour Mshawrab <mshawrabnour@gmail.com>
2022-05-24 09:43:54 +0300

    Launching game

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
 
+window.onload=function(){
+    var boundaries=document.getElementsByClassName("boundary");
+
+    document.getElementById("start").addEventListener("mouseenter", launchgame);
+
+
+}
